886db32490eb26e8a4bb32fdeec661fe301bf7bb
Enzo <enzode.simone@outlook.it>
2021-01-12 22:54:37 +0100

    update

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:check_app_version/show_dialog.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Check App Version',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Check App Version'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    ShowDialog(
+        context: context,
+        jsonUrl: 'https://spaziosalo.besimsoft.com/example.json',
+        updateButtonColor: Colors.blue,
+        laterButtonEnable: true)
+        .checkVersion();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(),
+    );
+
+  }
+}
