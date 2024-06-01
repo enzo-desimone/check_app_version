3bf657335f194de57739dde94eefa1b8d79f9388
crown.hg <crown.hg@gmail.com>
2024-06-01 11:55:31 +0800

    update package_info_plus: ^8.0.0; getJsonFile, data from, url, file, string; WillPopScope is deprecated in Flutter;

--- lib/repository/check_app_version.dart
+++ lib/repository/check_app_version.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
@@ -15,6 +16,88 @@ class CheckAppVersion {
 
   CheckAppVersion._internal();
 
+  bool _isJson(String input) {
+    try {
+      // final obj = json.decode(input);
+      final obj = json.decode(input);
+      if (obj is Map<String, dynamic>) {
+        return true;
+      }
+    } on FormatException {
+      return false;
+    }
+    return false;
+  }
+
+  bool _isUrl(String input) {
+    try {
+      Uri.parse(input);
+      return true;
+    } on FormatException {
+      return false;
+    }
+  }
+
+  bool _isFile(String input) {
+    try {
+      final uri = Uri.parse(input);
+      return uri.scheme == 'file';
+    } on FormatException {
+      return false;
+    }
+  }
+
+  /// getJsonFile, input from, url, file, string
+  ///
+  /// url,  http://127.0.0.1/version.json
+  /// file, file:///path/to/file.json
+  /// string, {"app_name": "", "new_app_version": "1.5.0", "new_app_code": "36"}
+  ///
+  Future<bool> getJsonFile(String input) async {
+    var raw = '';
+    if (_isUrl(input)) {
+      try {
+        final response = await http.get(Uri.parse(input));
+        if (response.statusCode == 200) {
+          raw = response.body;
+        }
+      } on Exception catch (error) {
+        log('Http error ${error.toString()}');
+        return false;
+      }
+    }
+
+    if (_isFile(input)) {
+      try {
+        final file = File(input);
+        if (await file.exists()) {
+          raw = await file.readAsString();
+        } else {
+          log('File notfound $input');
+          return false;
+        }
+      } catch (error) {
+        log('File error ${error.toString()}');
+        return false;
+      }
+    }
+
+    if (_isJson(input)) {
+      raw = input;
+    }
+
+    if (raw.isNotEmpty && _isJson(raw)) {
+      appFile = JsonFile.fromJson(json.decode(raw));
+      return true;
+    }
+
+    log('Input error: invalid input');
+    return false;
+
+    // ufn.lastline
+  }
+
+/*
   Future<bool> getJsonFile(String url) async {
     try {
       final response = await http.get(Uri.parse(url));
@@ -28,4 +111,7 @@ class CheckAppVersion {
       return false;
     }
   }
+*/
+
+  // cls.lastline
 }
